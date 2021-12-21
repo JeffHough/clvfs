fddd895485f07ec862ec0861cb3a22cfb980e966
JeffHough <hough552@gmail.com>
2021-12-20 21:52:32 -0500

    added inequality matrices function

--- TRMPC/return_cost_matrix.m
+++ TRMPC/return_cost_matrix.m
@@ -1,16 +1,16 @@
-function [C] = return_cost_matrix(u_con_mat, x_con_mat, Np, Q_final)
+function [C] = return_cost_matrix(u_cost_mat, x_cost_mat, Np, Q_final)
 
     % First, get the size of the input and state vectors:
-    u_dim = size(u_con_mat,2);
-    x_dim = size(x_con_mat,2);
+    u_dim = size(u_cost_mat,2);
+    x_dim = size(x_cost_mat,2);
 
     % Overall, there are Np-1 u-vectors and Np-1 x-vectors:
-    totalSize = (Np-1)*(u_dim + x_dim);
+    totalSize = Np*(u_dim + x_dim);
     C = zeros(totalSize);
 
     % Assign a combined input-state block:
-    Z = [u_con_mat,             zeros(u_dim, x_dim);
-         zeros(x_dim, u_dim),           x_con_mat];
+    Z = [u_cost_mat,             zeros(u_dim, x_dim);
+         zeros(x_dim, u_dim),           x_cost_mat];
 
     % Loop through populating the cost matrix:
     iDiag = 1;
